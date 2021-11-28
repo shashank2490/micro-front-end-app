a01e2517316557dc6a9f5955f80826fadb7272d1
Shashank <shashank@nagarro.com>
2021-11-28 17:05:52 +0530

    fix

--- src/GameComponent.jsx
+++ src/GameComponent.jsx
@@ -1,44 +1,59 @@
 import React, { useEffect } from "react";
 import styles from './index.module.scss';
 
-const GameComponent = ({gameName, componentHostUrl, history})=>{
+const stylesLoadingText={
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flex: '1 1 auto'
+}
 
-    useEffect(() => {
-        const scriptId = `micro-frontend-script-${gameName}`;
-    
-        const renderMicroFrontend = () => {
-          window[`render${gameName}`](`${gameName}-container`, history);
-        };
-    
-        if (document.getElementById(scriptId)) {
+const GameComponent = ({ gameName, componentHostUrl, history }) => {
+  const [showLoadingText, setShowLoadingText] = React.useState(true);
+
+  useEffect(() => {
+    const scriptId = `micro-frontend-script-${gameName}`;
+
+    const renderMicroFrontend = () => {
+      window[`render${gameName}`](`${gameName}-container`, history);
+    };
+
+    if (document.getElementById(scriptId)) {
+      renderMicroFrontend();
+      return;
+    }
+
+    fetch(`${componentHostUrl}/asset-manifest.json`)
+      .then((res) => res.json())
+      .then((manifest) => {
+        const style = document.createElement("link");
+        style.href = `${componentHostUrl}${manifest.files["main.css"]}`;
+        style.rel = `stylesheet`;
+        document.head.appendChild(style);
+
+        const script = document.createElement("script");
+        script.id = scriptId;
+        script.crossOrigin = "";
+        script.src = `${componentHostUrl}${manifest.files["main.js"]}`;
+        script.onload = () => {
+          setShowLoadingText(false);
           renderMicroFrontend();
-          return;
-        }
-        
-        fetch(`${componentHostUrl}/asset-manifest.json`)
-          .then((res) => res.json())
-          .then((manifest) => {
-            const style = document.createElement("link");
-            style.href = `${componentHostUrl}${manifest.files["main.css"]}`;
-            style.rel = `stylesheet`;
-            document.head.appendChild(style);
-
-            const script = document.createElement("script");
-            script.id = scriptId;
-            script.crossOrigin = "";
-            script.src = `${componentHostUrl}${manifest.files["main.js"]}`;
-            script.onload = () => {
-              renderMicroFrontend();
-            };
-            document.head.appendChild(script);
-          });
-    
-        return () => {
-          window[`unmount${gameName}`] && window[`unmount${gameName}`](`${gameName}-container`);
         };
+        document.head.appendChild(script);
       });
-    
-      return <div className={styles.fullWidth} id={`${gameName}-container`} />;
+
+    return () => {
+      window[`unmount${gameName}`] && window[`unmount${gameName}`](`${gameName}-container`);
+    };
+  });
+
+  return (
+    <>
+      {showLoadingText && <div style={stylesLoadingText}>Loading Game...</div>}
+      <div className={styles.fullWidth} id={`${gameName}-container`} />
+    </>
+  );
 }
 
 export default GameComponent;
